4e5490bbb27c6f936a05e24c1f224e15a3a9bc93
andy bravo <andres.bravo@propiedades.com>
2018-11-13 17:04:11 -0600

    🗒  Se agrega configuración para logs

--- src/main/java/com/andy/Application.java
+++ src/main/java/com/andy/Application.java
@@ -2,11 +2,16 @@ package com.andy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class Application {
 
+	private final static Logger logger = LoggerFactory.getLogger(Application.class);
+	
     public static void main(String[] args) {
+    	logger.info("Hola");
         SpringApplication.run(Application.class, args);
     }
 
